216089cfcfc7a0efcaf741509380e93864d0b7e2
p-vickerman <patrick@vickerman.net>
2022-06-28 11:38:24 -0400

    adding completed assignment

---
+++ main.js
@@ -0,0 +1,22 @@
+let paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+let heading = document.querySelector('h1');
+heading.style.fontSize = '48px';
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '.5'
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'I say, "Hi!"'
+
+let imageunderlist = document.querySelector('img');
+imageunderlist.src = 'https://static01.nyt.com/images/2016/08/05/us/05onfire1_xp/05onfire1_xp-superJumbo-v2.jpg'
+imageunderlist.style.height = '300px'
+
+newimage.src = 'https://loginportal.funnyjunk.com/pictures/This_9ddee7_5991597.jpg';
+newimage.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = "Won't get fooled again."
